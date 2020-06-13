3aa7e6458e589d6f036b99824b462b860867fa0f
Ricardo Colzani <6742811+rcolzani@users.noreply.github.com>
2020-06-13 15:51:46 -0300

    migrations and create transactions and categories

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,18 +1,33 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+import AppError from '../errors/AppError';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  return response.json({ ok: true });
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
+
+  if (type !== 'income' && type !== 'outcome') {
+    return response.status(400).json({ error: 'Invalid type' });
+  }
+
+  const createTransaction = new CreateTransactionService();
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,45 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const categories = getRepository(Category);
+    let categoryExistent = await categories.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryExistent) {
+      categoryExistent = categories.create({ title: category });
+      await categories.save(categoryExistent);
+    }
+
+    const transactions = getRepository(Transaction);
+    const transaction = transactions.create({
+      title,
+      value,
+      type,
+      category_id: categoryExistent.id,
+    });
+
+    transactions.save(transaction);
+
+    return transaction;
   }
 }
 
